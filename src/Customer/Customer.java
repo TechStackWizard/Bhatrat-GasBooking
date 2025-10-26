e90bea2451256c68c2d399259dbb47517ad5a714
TechStackWizard <anshullko474@gmail.com>
2025-10-26 22:27:09 +0530

    Update the main file with two new functions

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -3,11 +3,11 @@ package Customer;
 import gasSupplier.gasAgency;
 
 public class Customer {
-	String name;
-	String mobNumber;
-	String area;
-	String street;
-	int pinCode;
+	public String name;
+	public String mobNumber;
+	public String area;
+	public String street;
+	public int pinCode;
 	
 	public Customer(String name, String mobNumber, String area, String street, int pincode){
 		this.name = name;
--- src/com/example/main/Main.java
+++ src/com/example/main/Main.java
@@ -1,6 +1,57 @@
 package com.example.main;
 
+import Customer.*;
+import GasBooking.*;
+import gasSupplier.*;
+
 public class Main {
+	
+	static int count;
+	static int bcount=0 , ccount = 0, dcount = 0, pcount = 0;
+	
+	static String dpname;
+	
+	public static void cylinderCount(Delivery obj[]) {
+		String months[] = {
+			    "January", "February", "March", "April", "May", "June",
+			    "July", "August", "September", "October", "November", "December"
+			};
+		
+		for(Delivery delivery : obj) {
+			count = 0;
+			System.out.println("In the month of " + (months[delivery.dt_2.getMonth()] + ":"));
+			
+			System.out.println(" * In "+ delivery.area);
+			
+			if(delivery.status.equals("D")) {
+				count += delivery.numbersOfCylinders;
+				
+				System.out.println(" - " + count + " cylinder delivered...");
+				
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public static void checkLateDel (Delivery [] obj) {
+		String[] months = new String[]{"January", "february", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December" };
+		int[] month = new int[12];
+		for (Delivery delivery: obj) {\
+		if(delivery.status.equals("D") && delivery.amount 783.75 ){
+		month [delivery.dt_2.getMonth()] += 1;
+		/* february, month array at index 1st = 1; */
+		}
+		System.out.println(". -late delivery- ");
+		for (int i=0; i<12; i++){
+		if(month[i] 0){
+		System.out.println("* In " + months [i] + "there are " + month[i]);
+		}
+		}
+		System.out.println("\n");
+		}
+	
+	
     public static void main(String[] args) {
         System.out.println("Hello, Eclipse!");
     }
